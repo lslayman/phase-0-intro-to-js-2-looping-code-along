96516f4d13426f9d9ade0907ac7956df8f47d249
Lauren Slayman <laurenaslayman@gmail.com>
2023-03-06 13:43:58 -0700

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names, occasion) {
+  let cards=[]
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+  }
+  return cards;
+};
+console.log(writeCards(names, "surprise"));
+
+function countDown(){
+  let countDown = 10;
+  while (countDown > -1) {
+    console.log(countDown--);
+  }
+}
